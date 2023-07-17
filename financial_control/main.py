5a39d8fabd244ef3a547182a24d1693d258185f7
Aristides Neto <aristidesbneto@gmail.com>
2023-07-16 23:51:54 -0300

    Add cors para frontend

--- financial_control/main.py
+++ financial_control/main.py
@@ -1,11 +1,24 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from financial_control.core.config import settings
 from financial_control.apis.base import router
 
 app = FastAPI(title=settings.APP_NAME)
+
+origins = [
+    'http://localhost:5173'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
